e3e4b37ff441ab274a0a3481eb9d79fcb304780a
algonchoveriba <nigraalfa141@gmail.com>
2023-01-29 04:36:57 +0900

    ✨ supabase接続

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import { Layout } from "@/components/Layout";
+import type { NextPage } from "next";
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <Layout title="Home">
+      {'hello'}
+    </Layout>
+  )
+}
+
+export default Home
